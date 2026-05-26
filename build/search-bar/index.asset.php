7b99bfda773a38e7d1a06aa8c31fc26a85ddc4f6
lorettaborrelli-work <loretta.borrelli@mybreadcrumbs.it>
2026-05-26 17:23:04 +0200

    fix mobile nav

--- build/search-bar/index.asset.php
+++ build/search-bar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd145ad870598c7ce3a94');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e5a74cac4dc437c27d62');
